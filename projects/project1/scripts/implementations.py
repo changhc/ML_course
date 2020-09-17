317da9ac24ed02b5c45e0ec88942a46c481928b5
Huan-Cheng Chang <changhc84@gmail.com>
2020-09-17 23:06:49 +0200

    skeleton

---
+++ projects/project1/scripts/implementations.py
@@ -0,0 +1,17 @@
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    pass
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    pass
+
+def least_squares(y, tx):
+    pass
+
+def ridge_regression(y, tx, lambda_):
+    pass
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    pass
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    pass
